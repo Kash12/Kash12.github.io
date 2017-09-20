e7788c930169c21087cd82dd52cbde33c2956c2d
Kash12 <khashayar.kheiri@yahoo.com>
2017-09-21 01:21:33 +1000

    text & animation added

--- js/grayscale.js
+++ js/grayscale.js
@@ -97,3 +97,26 @@ $(document).ready(function() {
         });
     } 
 });
+
+
+// ============
+
+document.addEventListener('click', click);
+
+function click(e) {
+  let el
+  
+  el = e.target
+  
+  if (el !== e.currentTarget) {
+    if(el.nodeName === 'BUTTON') {
+      
+       if(el.classList.contains('is-active')) {
+         el.classList.remove('is-active')
+       } else {
+         el.classList.add('is-active')
+       }
+    }
+  }
+  event.stopPropagation()
+}
